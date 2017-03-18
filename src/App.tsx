afcf43157855178283991357c2e3f09d7a05568d
Mathew Allen <mathew.w.allen@gmail.com>
2017-03-18 03:45:07 -0400

    spruce up app layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,37 @@ import Home from './pages/Home';
 import ProxySheet from './pages/ProxySheet';
 
 const logo = require('./logo.svg');
+const heart = require('./heart.svg');
+const rhino = require('./rhino.jpg');
 
 class App extends React.Component<null, null> {
   render() {
     return (
       <div className={styles.App}>
-        <div className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
+        <header className={styles.Header}>
+          <img
+            src={rhino}
+            className={styles.Logo}
+            alt="A Rhino gazes lovingly at you, playfully licking the air."
+          />
           <h1>Rhoxy Proxy</h1>
-        </div>
+        </header>
+        <main className={styles.Content}>
+          <p className={styles.Intro}>
+            Proxies a rhino could love.
+          </p>
 
-        <p className={styles.AppIntro}>
-          Proxies a rhino could love.
-        </p>
-
-        <Router history={browserHistory}>
-          <Route path="/" component={Home}/>
-          <Route path="/proxy" component={ProxySheet}/>
-        </Router>
+          <Router history={browserHistory}>
+            <Route path="/" component={Home}/>
+            <Route path="/proxy" component={ProxySheet}/>
+          </Router>
+        </main>
+        <footer className={styles.Footer}>
+          <p >Made with
+            <img className={styles.FooterIcon} src={heart} alt="React" /> and
+            <img className={styles.FooterIcon} src={logo} alt="React" />
+          </p>
+        </footer>
       </div>
     );
   }
